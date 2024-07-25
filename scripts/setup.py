089569152f8cc411e245814088eea18fb255742e
pkradiator <kalalpriyanshu7@gmail.com>
2024-07-25 19:27:19 +0530

    Implemented the startup script

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,29 +1,31 @@
 import os
 
-
-
 def check_shell():
     try:
         shell = os.environ['SHELL']
-        if shell.find('bash'):
-            profile = '~/.bashrc'
-            logout_profile = '~/.bash_logout'
-        elif shell.find('zsh'):
-            profile = '~/.zshrc'
-            logout_profile = '~/.zlogout'
-        else :
-            print("Cannot automatically detect shell (terminal), need to manually add config on your shell profile!!")
-            profile = None
-            logout_profile = None
+        if shell.find('bash')!= -1:
+            profile = os.path.expanduser('~/.bashrc')
+            logout_profile = os.path.expanduser('~/.bash_logout')
+        elif shell.find('zsh')!= -1:
+            profile = os.path.expanduser('~/.zshrc')
+            logout_profile = os.path.expanduser('~/.zlogout')
 
-    except :
-            print("$SHELL evironment variable could not be found!!\nNeed to manually add config on your shell profile")
-    
+        script = '\n# added by multigit\nsource $HOME/.multigit\n'
+        logout_script = '\n# added by multigit\nssh-add -D\neval $(ssh-agent -k)\ngit config --global --unset user.name\ngit config --global --unset user.email\n'
+        if shell.find('bash') != -1 or shell.find('zsh') != -1:
+            with open(profile, 'a+') as f, open(logout_profile, 'a+') as g:
+                f.seek(0)
+                if script in f.read():
+                    print('Setup is already complete')
+                else:
+                    f.seek(0,2)
+                    f.write(script)
+                    g.write(logout_script)
 
-    
-
-    
-
+    except KeyError:
+        print("Cannot automatically detect shell (terminal), need to manually add config on your shell profile!!")
+        profile = None
+        logout_profile = None
 
 
 
